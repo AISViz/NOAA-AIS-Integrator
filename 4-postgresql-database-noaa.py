3e08ec9b2d91162a37306468b539e45373552eb4
Ruixin Song <tsuzsrx@gmail.com>
2025-01-17 16:46:14 -0400

    Update 4-postgresql-database-noaa.py
    Remove db user info

--- 4-postgresql-database-noaa.py
+++ 4-postgresql-database-noaa.py
@@ -1,15 +1,14 @@
 import aisdb
 
 # psql connection string
-USER = 'ruixin'
-PASSWORD = 'ruixin123'
+USER = 'psql_username'
+PASSWORD = 'psql_password'
 ADDRESS = '127.0.0.1'
 PORT = 5432
-DBNAME = 'noaa'
+DBNAME = 'psql_dbname'
 psql_conn_string = f"postgresql://{USER}:{PASSWORD}@{ADDRESS}:{PORT}/{DBNAME}"
 
 filepaths = aisdb.glob_files('merged/','.csv')
-filepaths = sorted([f for f in filepaths if '202410' in f])
 print(f'Number of files: {len(filepaths)}')
 
 with aisdb.PostgresDBConn(libpq_connstring=psql_conn_string) as dbconn:
