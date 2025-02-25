74b59fa4242297db5f50fd7edd1b8376e3d9b019
rui <tsuzsrx@gmail.com>
2025-02-25 10:38:48 -0400

    Enhance NOAA pipeline

--- 4-postgresql-database-noaa.py
+++ 4-postgresql-database-noaa.py
@@ -1,20 +1,26 @@
 import aisdb
 
 # psql connection string
-USER = 'psql_username'
-PASSWORD = 'psql_password'
+USER = 'ruixin'
+PASSWORD = 'ruixin123'
 ADDRESS = '127.0.0.1'
 PORT = 5432
-DBNAME = 'psql_dbname'
+DBNAME = 'noaa'
 psql_conn_string = f"postgresql://{USER}:{PASSWORD}@{ADDRESS}:{PORT}/{DBNAME}"
 
-filepaths = aisdb.glob_files('merged/','.csv')
+filepaths = aisdb.glob_files('/slow-array/NOAA/test/*', '.zip')
+print(filepaths)
 print(f'Number of files: {len(filepaths)}')
 
 with aisdb.PostgresDBConn(libpq_connstring=psql_conn_string) as dbconn:
-    aisdb.decode_msgs(filepaths,
-                    dbconn=dbconn,
-                    source='NOAA',
-                    verbose=True,
-                    skip_checksum=False)
-    
+    try:
+        aisdb.decode_msgs(filepaths,
+                        dbconn=dbconn,
+                        source='NOAA',
+                        verbose=True,
+                        skip_checksum=True,
+                        raw_insertion=True,
+                        workers=6,
+                        timescaledb=True)
+    except Exception as e:
+        print(f'Error loading: {e}')
